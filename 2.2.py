fabc58d7d687ee410077d5b5b1aff5a894464636
tanaporn-p <78866917+tanaporn-p@users.noreply.github.com>
2021-02-11 00:51:47 +0700

    Create 2.2.py

---
+++ 2.2.py
@@ -0,0 +1,6 @@
+# pythontanaporn
+print('Day Converter Program')
+a=int(input('input number of Days --> '))
+print("%d day  ---> Hour "%a, a*24,"Hours")
+print("%d day ---> Minutes "%a, a*1440,"Minutes")
+print("%d day  ---Seconds "%a, a*86400,"Seconds")
